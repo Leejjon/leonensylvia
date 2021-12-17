b5856062c32e359dc7578fc7377ceaf6242619ee
Leejjon <leejjon@gmail.com>
2021-12-17 23:28:52 +0100

    Put a hello world backend in place.

---
+++ backend/webpackdev.config.ts
@@ -0,0 +1,29 @@
+const webpack = require("webpack");
+const pathDev = require("path");
+const nodeExternalsDev = require("webpack-node-externals");
+const copyFilesDev = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: ["webpack/hot/poll?100", './src/index.ts'],
+    target: "node",
+    mode: "development",
+    externals: [
+        nodeExternalsDev({
+            allowlist: ["webpack/hot/poll?100"]
+        })
+    ],
+    resolve: {
+        extensions: ['.ts', ".js"]
+    },
+    module: {
+        rules: [
+            // all files with a `.ts` extension will be handled by `ts-loader`
+            { test: /\.ts?$/, loader: 'ts-loader', exclude: /node_modules/}
+        ]
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin() /*, new copyFilesDev({ patterns: [{ from: '../blindpool-client/build', to: 'build'}]})*/],
+    output: {
+        path: pathDev.resolve(__dirname, 'dist'),
+        filename: 'index.js'
+    },
+};
